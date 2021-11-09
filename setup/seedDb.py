ce5b0b75d43817ecbc5c5b78e7e14239c438f246
Tom Welch <Tom.Welch@impsoftware.co.uk>
2021-11-09 15:08:28 +0000

    Add Database Seed Scripts

---
+++ setup/seedDb.py
@@ -0,0 +1,17 @@
+import os
+import pandas as pd
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+load_dotenv() #Loading environment variables
+
+#Loading data into Pandas DataFrame ---
+
+df = pd.read_csv("./employees.csv")
+
+#Seeding data to DB ---
+
+DB_URI = f"postgresql+psycopg2://{os.getenv("PG_USER")}:{os.getenv("PG_PASSWORD")}@{os.getenv("PG_HOST")}:{os.getenv("PG_PORT")}/{os.getenv("PG_DB")}"
+engine = create_engine(DB_URI)
+
+df.to_sql("Employees", con = engine, if_exists = "replace")
